9c75a6397dc7f80ef04605d915aec207d5a3d0eb
Goncalocp <goncalocp02@gmail.com>
2024-04-11 13:48:04 +0100

    update

--- app/src/main/java/com/example/braguia/ui/MainActivity.java
+++ app/src/main/java/com/example/braguia/ui/MainActivity.java
@@ -1,33 +1,63 @@
 package com.example.braguia.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ImageButton;
-import android.widget.Toast;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-import androidx.recyclerview.widget.RecyclerView;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentController;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.example.braguia.R;
+import com.example.braguia.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    ImageButton imgButton;
+    ActivityMainBinding binding;
+    Button seeMapButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        RecyclerView rv = findViewById(R.id.recyclerview);
-        //final UserRecyclerViewAdapter userAdapter = new UserRecyclerViewAdapter()
-        //ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-        //    Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-        //    v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-        //    return insets;
-        //});
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        replaceFragment(new HomeFragment());
+
+
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            if (item.getItemId() == R.id.home) {
+                replaceFragment(new HomeFragment());
+            } else if (item.getItemId() == R.id.map){
+                replaceFragment(new MapFragment());
+            } else if (item.getItemId() == R.id.favourite){
+                replaceFragment(new FavouritesFragment());
+            } else if (item.getItemId() == R.id.profile){
+                replaceFragment(new ProfileFragment());
+            }
+            return true;
+        });
+    }
+
+    private void replaceFragment(Fragment fragment){
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frame_layout,fragment);
+        fragmentTransaction.commit();
     }
 }
