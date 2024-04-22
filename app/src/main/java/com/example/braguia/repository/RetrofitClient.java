aeed59c667eda6e3e3f785f6594852bc1b009907
Goncalocp <goncalocp02@gmail.com>
2024-04-22 16:35:55 +0100

    update

---
+++ app/src/main/java/com/example/braguia/repository/RetrofitClient.java
@@ -0,0 +1,19 @@
+package com.example.braguia.repository;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitClient {
+    private static Retrofit retrofit;
+    private static final String BASE_URL = "https://579f8a2e08b3ae26545741d09e8f230a.serveo.net/";
+
+    public static Retrofit getRetrofitInstance() {
+        if (retrofit == null) {
+            retrofit = new retrofit2.Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+        }
+        return retrofit;
+    }
+}
